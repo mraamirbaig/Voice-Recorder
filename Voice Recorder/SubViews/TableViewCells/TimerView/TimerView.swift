e3e19573cea36eb422e736a38cbf3587eb17413c
“Aamir <abaig@wallpostdev.com>
2017-07-02 10:33:42 +0300

    By Amir for creating TimerView

---
+++ Voice Recorder/SubViews/TableViewCells/TimerView/TimerView.swift
@@ -0,0 +1,35 @@
+//
+//  TimerView.swift
+//  Voice Recorder
+//
+//  Created by IT O on 7/2/17.
+//  Copyright © 2017 AamirBaig. All rights reserved.
+//
+
+import UIKit
+
+class TimerView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
