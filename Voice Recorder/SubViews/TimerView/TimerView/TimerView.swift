0717cc155f7744fd46eb736e2d400d158d928b8f
“Aamir <abaig@wallpostdev.com>
2017-07-02 13:17:49 +0300

    By Amir

--- Voice Recorder/SubViews/TimerView/TimerView/TimerView.swift
+++ Voice Recorder/SubViews/TimerView/TimerView/TimerView.swift
@@ -40,29 +40,7 @@ class TimerView: UIView {
         
         //Find the difference between current time and start time.
         let elapsedTime: TimeInterval = currentTime - startTime
-        
-//        //calculate the minutes in elapsed time.
-//        let minutes = UInt8(elapsedTime / 60.0)
-//        elapsedTime -= (TimeInterval(minutes) * 60)
-//        
-//        //calculate the seconds in elapsed time.
-//        let seconds = UInt8(elapsedTime)
-//        elapsedTime -= TimeInterval(seconds)
-//        
-//        //find out the fraction of milliseconds to be displayed.
-//        let fraction = UInt8(elapsedTime * 100)
-//        
-//        //add the leading zero for minutes, seconds and millseconds and store them as string constants
-//        
-//        let strMinutes = String(format: "%02d", minutes)
-//        let strSeconds = String(format: "%02d", seconds)
-//        let strFraction = String(format: "%02d", fraction)
-//
-//        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
-//
-//        print("\(strMinutes):\(strSeconds):\(strFraction)")
-        
-        //print("timer..\(DateTimeService().getDurationStringFromTimeInterval(elapsedTime))")
+ 
         timerLabel.text = DateTimeService().getDurationStringFromTimeInterval(elapsedTime)
     }
     
